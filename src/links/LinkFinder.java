d9cb7a8fda139e69bd7ee083873e88498b0bd8a0
slanger <s.mark.langer@outlook.com>
2014-04-10 16:07:42 -0400

    LinkFinder implemented

--- src/links/LinkFinder.java
+++ src/links/LinkFinder.java
@@ -1,6 +1,15 @@
 package links;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.PriorityBlockingQueue;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import util.PageInfo;
 
@@ -23,8 +32,73 @@ public class LinkFinder
 	{
 		if (pageInfo.remainingHops > 0)
 		{
-			// TODO get file from pageInfo.fileName
-			// TODO for each link in file, add link to outboundQueue
+			// get file from pageInfo.fileName
+			File webpage = new File(pageInfo.fileName);
+			Document doc;
+			try
+			{
+				doc = Jsoup.parse(webpage, "UTF-8", pageInfo.url.toString());
+			}
+			catch (IOException e)
+			{
+				throw new RuntimeException(e);
+			}
+
+			// for each link in file, add link to outboundQueue
+
+			// <a> links
+			Elements aElements = doc.getElementsByTag("a");
+			for (int i = 0; i < aElements.size(); i++)
+			{
+				Element e = aElements.get(i);
+				String linkString = e.attr("href");
+				PageInfo pi;
+				try
+				{
+					pi = new PageInfo(new URL(linkString), pageInfo.remainingHops - 1);
+				}
+				catch (MalformedURLException e1)
+				{
+					throw new RuntimeException(e1);
+				}
+				outboundQueue.add(pi);
+			}
+
+			// <nav> links
+			Elements navElements = doc.getElementsByTag("nav");
+			for (int i = 0; i < navElements.size(); i++)
+			{
+				Element e = navElements.get(i);
+				String linkString = e.attr("href");
+				PageInfo pi;
+				try
+				{
+					pi = new PageInfo(new URL(linkString), pageInfo.remainingHops - 1);
+				}
+				catch (MalformedURLException e1)
+				{
+					throw new RuntimeException(e1);
+				}
+				outboundQueue.add(pi);
+			}
+
+			// <link> links
+			Elements linkElements = doc.getElementsByTag("link");
+			for (int i = 0; i < linkElements.size(); i++)
+			{
+				Element e = linkElements.get(i);
+				String linkString = e.attr("href");
+				PageInfo pi;
+				try
+				{
+					pi = new PageInfo(new URL(linkString), pageInfo.remainingHops - 1);
+				}
+				catch (MalformedURLException e1)
+				{
+					throw new RuntimeException(e1);
+				}
+				outboundQueue.add(pi);
+			}
 		}
 
 		// TODO let the delete task know that we are done finding links in file
